64e78e540a787325d07e816d333d8eac78818dfc
susanwen4no <56980359+susanwen4no@users.noreply.github.com>
2021-05-02 14:02:33 -0400

    Add files via upload

--- p6/flask_app/config.py
+++ p6/flask_app/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Stores all configuration values
-SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+SECRET_KEY = os.getenv("SECRET_KEY") #b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
 MONGODB_HOST = os.getenv("MONGODB_HOST")
 
 MAIL_SERVER = 'smtp.gmail.com'
@@ -9,4 +9,4 @@ MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_DEFAULT_SENDER = 'projectdiary388j@gmail.com'
 MAIL_USERNAME = 'projectdiary388j@gmail.com'
-MAIL_PASSWORD = 'tasbcbuxdwulngpp' #cmscconfirm123
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD") #'tasbcbuxdwulngpp' #cmscconfirm123
