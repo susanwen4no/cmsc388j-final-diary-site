57914f8401d819f98e8ba494af81644a0fa37d40
susanwen4no <56980359+susanwen4no@users.noreply.github.com>
2021-05-02 13:35:11 -0400

    Add files via upload

--- p6/flask_app/config.py
+++ p6/flask_app/config.py
@@ -2,7 +2,7 @@ import os
 
 # Stores all configuration values
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = os.getenv("MONGODB_HOST")
+app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
